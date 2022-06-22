a85082a3bdea2a415efc72e3cb12d249c6d64652
Ian-Marco-Moffett <spurious.vector@gmail.com>
2022-06-22 13:13:41 -0400

    feat: Added more features to enum.

--- include/cpuinfo.h
+++ include/cpuinfo.h
@@ -55,6 +55,11 @@ typedef enum
 	CPU_MMX 		= 	(1 << 23), 			// MMX.
 	CPU_FXSR		= 	(1 << 24), 			// FXSAVE and FXSTOR instructions.
 	CPU_SSE 		= 	(1 << 25), 			// Streaming SMID extensions.
+	CPU_SSE2 		= 	(1 << 26), 			// Streaming SIMD extensions.
+	CPU_SS			=	(1 << 27), 			// Self snoop.
+	CPU_HTT			=	(1 << 28),			// Multi-threading.
+	CPU_TM			=	(1 << 29),			// Thermal monitor.
+	CPU_PBE			=	(1 << 31),			// Pending break enable.
 } CPU_FEATURE;
 
 
